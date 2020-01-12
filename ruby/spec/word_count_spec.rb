643653a1e7c76e3b3a63673644ee180d793b677f
projectormato <k.keita811.anuy@gmail.com>
2020-01-12 15:10:57 +0900

    feat: Ruby版WordCount実装

--- ruby/spec/word_count_spec.rb
+++ ruby/spec/word_count_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe 'Word Count' do
     expected = {'hello' => 1}
     expect(WordCount.new.calc('hello')).to eq expected
   end
+
+  it 'input two hello' do
+    expected = {'hello' => 2}
+    expect(WordCount.new.calc('hello hello')).to eq expected
+  end
+
+  it 'input hello world' do
+    expected = {'hello' => 1, 'world' => 1}
+    expect(WordCount.new.calc('hello world')).to eq expected
+  end
 end
