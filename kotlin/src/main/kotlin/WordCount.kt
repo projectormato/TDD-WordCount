b1d97b77d36e086c8b48f27239e29a407a6e5567
projectormato <k.keita811.anuy@gmail.com>
2020-01-12 23:53:01 +0900

    refactor: groupingByを使用してリファクタリング

--- kotlin/src/main/kotlin/WordCount.kt
+++ kotlin/src/main/kotlin/WordCount.kt
@@ -2,14 +2,6 @@ package main.java
 
 class WordCount {
     fun calc(input: String): Map<String, Int> {
-        val result = mutableMapOf<String, Int>()
-        for (s in input.split(" ")) {
-            if (result[s] != null) {
-                result[s] = result[s]!!.plus(1)
-            } else {
-                result[s] = 1
-            }
-        }
-        return result
+        return input.split(" ").groupingBy { it }.eachCount()
     }
 }
