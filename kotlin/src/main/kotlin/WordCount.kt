400ce53f150381227c9f5ac72e85fe2fab306912
projectormato <k.keita811.anuy@gmail.com>
2020-01-12 23:51:10 +0900

    refactor: 分離していた処理を統一

--- kotlin/src/main/kotlin/WordCount.kt
+++ kotlin/src/main/kotlin/WordCount.kt
@@ -2,12 +2,13 @@ package main.java
 
 class WordCount {
     fun calc(input: String): Map<String, Int> {
-        if (input == "hello hello") {
-            return mapOf("hello" to input.split(" ").size)
-        }
         val result = mutableMapOf<String, Int>()
         for (s in input.split(" ")) {
-            result[s] = 1
+            if (result[s] != null) {
+                result[s] = result[s]!!.plus(1)
+            } else {
+                result[s] = 1
+            }
         }
         return result
     }
